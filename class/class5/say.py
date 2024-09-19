baa13fe1ebec6734390fba5cdb2deb1db9fc371f
Haruka Banin <haru.banin@gmail.com>
2024-09-19 13:37:43 -0700

    class5

--- class/class5/say.py
+++ class/class5/say.py
@@ -5,3 +5,8 @@ import sys
 
 if len(sys.argv) == 2:
     cowsay.cow("hello, " + sys.argv[1])
+
+from sayings import hello
+
+if len(sys.argv) == 2:
+    hello(sys.argv[1])
---
+++ class/class5/sayings.py
@@ -0,0 +1,11 @@
+def hello(name):
+    print(f"hello, {name}")
+
+
+def goodbye(name):
+    print(f"goodbye, {name}")
+
+
+if __name__ == "__main__":
+    main()
+
